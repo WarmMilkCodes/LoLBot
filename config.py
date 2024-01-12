8c4202c738e0c57f21dd91e526c7f36e61acb43a
WarmMilkCodes <mrguybanks@gmail.com>
2024-01-12 17:19:49 -0600

    Update

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 token = "MTE3MTIwMjM3ODAyNDg4MjE4Ng.GBmeq5.k5eaQHx6E0unZjowE1SUx0F56BE4DgO-fGz6FQ"
 
 MongoClient = "mongodb+srv://dylncox:NEWpass1@cluster0.ifyjv.mongodb.net/cluster0?retryWrites=true&w=majority"
-db = "URLoL"
+db = "URLOL"
 player_collection = "players"
 replay_collection = "replays"
 team_collection = "teams"
 
+riot_api = "RGAPI-2b7a24a2-a59b-475d-8966-2417e78bc95a"
+
+bot_admin_channel = 1171263860716601366
+
+riot_id_log_channel = 1194430443362205767
