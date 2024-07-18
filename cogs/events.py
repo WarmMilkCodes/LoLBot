8ef46cb21fc9ec1a6e60c63b460adf21c794ded5
WarmMilkCodes <dylncox@gmail.com>
2024-07-18 14:19:14 -0500

    Add logging

--- cogs/events.py
+++ cogs/events.py
@@ -12,7 +12,7 @@ class EventsCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        # Check and add members to database
+        logger.info("Bot is ready. Checking and adding members to the database.")
         for member in self.bot.get_all_members():
             if not member.bot:
                 self.add_member_to_db(member)
@@ -30,16 +30,20 @@ class EventsCog(commands.Cog):
                 "rank": None,
                 "joined_at": datetime.utcnow()
             })
-            logger.info(f"Added {member.name} to database.")
+            logger.info(f"Added {member.name} ({member.id}) to database.")
+        else:
+            logger.info(f"Member {member.name} ({member.id}) already exists in database.")
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         if member.bot:
             return
         
+        logger.info(f"New member joined: {member.name} ({member.id}). Adding to database.")
         # Add member to database
         self.add_member_to_db(member)
 
+        logger.info(f"Notifying admin channel about new member: {member.name} ({member.id}).")
         # Notify in admin channel
         await self.notify_admin_channel(member)
 
@@ -52,9 +56,17 @@ class EventsCog(commands.Cog):
         embed.set_thumbnail(url=member_pfp)
 
         admin_channel = config.bot_admin_channel
-        await self.bot.get_channel(admin_channel).send(embed=embed)
+        channel = self.bot.get_channel(admin_channel)
+
+        if channel:
+            await channel.send(embed=embed)
+            logger.info(f"Admin channel notified about new member: {member.name} ({member.id}).")
+        else:
+            logger.error(f"Admin channel with ID {admin_channel} not found.")
 
 
 def setup(bot):
+    logger.info("Setting up EventsCog...")
     bot.add_cog(EventsCog(bot))
+    logger.info("EventsCog setup completed.")
 
