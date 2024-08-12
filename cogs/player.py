243fec3d97cf1fb47cc30370bd84f186e461a515
WarmMilkCodes <mrguybanks@gmail.com>
2024-08-11 20:02:27 -0500

    Remove unneccesary checks, add Riot ID error send to failure channel.

--- cogs/player.py
+++ cogs/player.py
@@ -41,13 +41,15 @@ class PlayerCog(commands.Cog):
     ## Function to update ranks for all players
     async def update_all_ranks(self):
         logger.info("Updating ranks for all players.")
-        #failure_channel = self.bot.get_channel(config.failure_log_channel)
+        failure_channel = self.bot.get_channel(config.failure_log_channel)
         players = dbInfo.player_collection.find({})
+
         for player in players:
             logger.info(f"Processing player: {player['name']}")
             if 'game_name' in player and 'tag_line' in player:
                 puuid = await self.get_puuid(player['game_name'], player['tag_line'])
-                if puuid:
+
+                if puuid and (not player.get('puuid') or player.get('puuid') != puuid):
                     dbInfo.player_collection.update_one(
                         {"discord_id": player['discord_id']},
                         {"$set": {"puuid": puuid}}
@@ -55,7 +57,7 @@ class PlayerCog(commands.Cog):
                     logger.info(f"Stored PUUID for {player['name']}")
 
                     summoner_id = await self.get_summoner_id(puuid)
-                    if summoner_id:
+                    if summoner_id and (not player.get('summoner_id') or player.get('summoner_id') != summoner_id):
                         dbInfo.player_collection.update_one(
                             {"discord_id": player['discord_id']},
                             {"$set": {"summoner_id": summoner_id}}
@@ -63,46 +65,42 @@ class PlayerCog(commands.Cog):
                         logger.info(f"Stored Summoner ID for player {player['name']}")
 
                         rank_info = await self.get_player_rank(summoner_id)
-                        if rank_info:
-                            # Store rank information with date
-                            date_str = datetime.now(pytz.utc).strftime('%m-%d-%Y')
-                            historical_rank_info = player.get('historical_rank_info', {})
-                            
-                            # Check if rank info for today already exists and is the same
-                            if date_str in historical_rank_info and historical_rank_info[date_str] == rank_info:
-                                logger.info(f"Rank information for player {player['name']} is unchanged for today.")
-                                continue
+                if rank_info:
+                    # Store rank information with date
+                    date_str = datetime.now(pytz.utc).strftime('%m-%d-%Y')
+                    historical_rank_info = player.get('historical_rank_info', {})
 
-                            historical_rank_info[date_str] = rank_info
+                    # Check if rank info for today already exists and is the same
+                    if date_str in historical_rank_info and historical_rank_info[date_str] == rank_info:
+                        logger.info(f"Rank information for player {player['name']} is unchanged for today.")
+                        continue
 
-                            dbInfo.player_collection.update_one(
-                                {"discord_id": player['discord_id']},
-                                {"$set": {
-                                    "rank_info": rank_info, 
-                                    "last_updated": datetime.now(pytz.utc).strftime('%m-%d-%Y'),
-                                    "historical_rank_info": historical_rank_info
-                                    }}
-                            )
-                            logger.info(f"Updated rank information for player {player['name']} and set last updated.")
+                    # If current rank_info is different, update the database
+                    if not player.get('rank_info') or player['rank_info'] != rank_info:
+                        historical_rank_info[date_str] = rank_info
 
-                        else:
-                            dbInfo.player_collection.update_one(
-                                {"discord_id": player['discord_id']},
-                                {"$set": {"last_updated":datetime.now(pytz.utc).strftime('%m-%d-%Y')}}
-                            )
-                            #await self.report_failure(failure_channel, player['name'], "Failed to retrieve rank information")
-                            logger.warning(f"No rank information found for {player['name']}, but updated last_updated")
-                            
-                    else:
-                        #await self.report_failure(failure_channel, player['name'], "Failed to retrieve summoner information")
-                        logger.warning(f"Failed to retrieve summoner information for {player['name']}")
+                        dbInfo.player_collection.update_one(
+                            {"discord_id": player['discord_id']},
+                            {"$set": {
+                                "rank_info": rank_info, 
+                                "last_updated": datetime.now(pytz.utc).strftime('%m-%d-%Y'),
+                                "historical_rank_info": historical_rank_info
+                                }}
+                        )
+                        logger.info(f"Updated rank information for player {player['name']} and set last updated.")
                 else:
-                    #await self.report_failure(failure_channel, player['name'], f"Failed to retrieve PUUID for {player['game_name']}#{player['tag_line']}")
-                    logger.warning(f"Failed to retrieve PUUID for {player['game_name']}#{player['tag_line']}")
+                    if not player.get('last_updated') or player['last_updated'] != datetime.now(pytz.utc).strftime('%m-%d-%Y'):
+                        dbInfo.player_collection.update_one(
+                            {"discord_id": player['discord_id']},
+                            {"$set": {"last_updated":datetime.now(pytz.utc).strftime('%m-%d-%Y')}}
+                        )
+                    logger.warning(f"No rank information found for {player['name']}, but updated last_updated")
+                    
             else:
-                #await self.report_failure(failure_channel, player['name'], "Player does not have game_name and tag_line set")
                 logger.error(f"Player {player['name']} does not have game_name and tag_line set.")
+                await self.report_failure(failure_channel, player['name'], 'Riot ID is not set or is invalid.')
         logger.info("Completed updating ranks for all players.")
+
 
     ## Function to get PUUID
     async def get_puuid(self, game_name, tag_line):
@@ -157,13 +155,8 @@ class PlayerCog(commands.Cog):
     ## Function to report failures to a specific channel
     async def report_failure(self, channel, player_name, reason):
         if channel:
-            embed = discord.Embed(
-                title="Player Rank Update Failure",
-                description=f"**Player:** {player_name}\n**Reason:** {reason}",
-                color=discord.Color.red(),
-                timestamp=datetime.now(pytz.utc)
-            )
-            await channel.send(embed=embed)
+            message = f"Player: {player_name} | Reason: {reason}"
+            await channel.send(message)
 
 def setup(bot):
     bot.add_cog(PlayerCog(bot))
