080c9e1cbc704c817be7c9939c9caadbee07d9b4
WarmMilkCodes <mrguybanks@gmail.com>
2024-08-18 19:24:59 -0500

    update

--- cogs/player.py
+++ cogs/player.py
@@ -1,4 +1,5 @@
-import aiohttp, discord
+import aiohttp
+import discord
 import logging
 import pytz
 from datetime import datetime, timezone
@@ -52,7 +53,6 @@ class PlayerCog(commands.Cog):
         else:
             await ctx.respond("Eligibility check task is already running.", ephemeral=True)
 
-
     @tasks.loop(hours=24)
     async def rank_update_task(self):
         logger.info("Rank update task triggered.")
@@ -63,17 +63,15 @@ class PlayerCog(commands.Cog):
         logger.info("Eligibility check task triggered.")
         await self.check_player_eligibility()
 
-
     @commands.slash_command(guild_ids=[config.lol_server], description="Update player ranks manually")
     @commands.has_permissions(administrator=True)
     async def update_ranks(self, ctx):
-        await ctx.defer()
+        await ctx.defer(ephemeral=True)
         await self.update_all_ranks()
         await ctx.respond("Updated ranks for all players", ephemeral=True)
 
     async def update_all_ranks(self):
         logger.info("Updating ranks for all players.")
-        failure_channel = self.bot.get_channel(config.failure_log_channel)
         players = dbInfo.intent_collection.find({"Playing": "Yes"})
 
         for player in players:
@@ -95,12 +93,8 @@ class PlayerCog(commands.Cog):
                         )
                         logger.info(f"Stored PUUID for {player_record['name']}")
                 else:
-                    # Fallback check for existing rank data
                     if player_record.get('rank_info'):
                         logger.warning(f"Failed to retrieve PUUID for {player_record['name']}. Using stored rank info.")
-                    else:
-                        await self.report_failure(failure_channel, player_record['name'], 'Failed to retrieve PUUID')
-                        await self.assign_not_eligible_role(player_record['discord_id'])
                     continue
 
                 summoner_id = await self.get_summoner_id(puuid)
@@ -112,12 +106,8 @@ class PlayerCog(commands.Cog):
                         )
                         logger.info(f"Stored Summoner ID for player {player_record['name']}")
                 else:
-                    # Fallback check for existing rank data
                     if player_record.get('rank_info'):
                         logger.warning(f"Failed to retrieve Summoner ID for {player_record['name']}. Using stored rank info.")
-                    else:
-                        await self.report_failure(failure_channel, player_record['name'], 'Failed to retrieve Summoner ID')
-                        await self.assign_not_eligible_role(player_record['discord_id'])
                     continue
 
                 rank_info = await self.get_player_rank(summoner_id)
@@ -141,23 +131,15 @@ class PlayerCog(commands.Cog):
                         )
                         logger.info(f"Updated rank information for player {player_record['name']} and set last updated.")
                 else:
-                    # Fallback check for existing rank data
                     if player_record.get('rank_info'):
                         logger.warning(f"Unable to retrieve rank info for {player_record['name']} with API. Using stored rank info.")
-                    else:
-                        await self.report_failure(failure_channel, player_record['name'], 'Unable to retrieve rank info')
-                        await self.assign_not_eligible_role(player_record['discord_id'])
-                        dbInfo.player_collection.update_one({"discord_id": player_record['discord_id']}, {"$set": {"eligible": "False"}})
             else:
                 logger.error(f"Player {player_record['name']} does not have game_name and tag_line set.")
-                await self.report_failure(failure_channel, player_record['name'], 'Riot ID is not set or is invalid.')
-                await self.assign_not_eligible_role(player_record['discord_id'])
 
         logger.info("Completed updating ranks for all players.")
 
     async def check_player_eligibility(self):
         logger.info("Checking player eligibility for the current split.")
-        failure_channel = self.bot.get_channel(config.failure_log_channel)
         players = dbInfo.intent_collection.find({"Playing": "Yes"})
 
         for player in players:
@@ -179,16 +161,20 @@ class PlayerCog(commands.Cog):
 
             if len(eligible_games) >= 30:
                 logger.info(f"Player {player_record['name']} has met the eligibility requirements.")
-                # Update eligibility in database or notify
+                dbInfo.player_collection.update_one(
+                    {"discord_id": player_record['discord_id']},
+                    {"$set": {"eligible_for_split": True, "last_eligibility_check": datetime.now(pytz.utc)}}
+                )
             else:
                 logger.warning(f"Player {player_record['name']} has not met the eligibility requirements.")
-                await self.report_failure(failure_channel, player_record['name'], 'Player has not met the minimum game requirement for the split.')
-                #await self.assign_not_eligible_role(player_record['discord_id'])
+                dbInfo.player_collection.update_one(
+                    {"discord_id": player_record['discord_id']},
+                    {"$set": {"eligible_for_split": False, "last_eligibility_check": datetime.now(pytz.utc)}}
+                )
 
         logger.info("Completed checking eligibility for all players.")
 
     async def get_match_history(self, puuid):
-        # Implement API call to fetch match history
         url = f"https://<REGION>.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids"
         headers = {'X-Riot-Token': config.RIOT_API}
         async with aiohttp.ClientSession() as session:
@@ -199,50 +185,22 @@ class PlayerCog(commands.Cog):
                     logger.error(f"Error fetching match history for PUUID {puuid}: {await response.text()}")
                     return []
                 
-    async def is_eligible_match(self, summoner_id):
-        """Check if the player has played the minimum number of matches in the current split."""
+    async def is_eligible_match(self, match):
         current_split = next((split for split in SPLITS if split['start'] <= datetime.now(timezone.utc) and (split['end'] is None or split['end'] >= datetime.now(timezone.utc))), None)
         
         if not current_split:
             logger.error("No active split found.")
             return False
 
-        start_timestamp = int(current_split['start'].timestamp() * 1000)  # Convert to milliseconds
-        match_history_url = f"https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/{summoner_id}?beginTime={start_timestamp}"
-
-        headers = {'X-Riot-Token': config.RIOT_API}
-        
-        async with aiohttp.ClientSession() as session:
-            async with session.get(match_history_url, headers=headers) as response:
-                if response.status == 200:
-                    match_history = await response.json()
-                    total_matches = len(match_history.get('matches', []))
-                    logger.info(f"Player with summoner ID {summoner_id} has played {total_matches} matches in the current split.")
-                    return total_matches >= 30  # Check if the player has played at least 30 matches
-                else:
-                    logger.error(f"Error fetching match history for Summoner ID {summoner_id}: {await response.text()}")
-                    return False
-
-
-    async def assign_not_eligible_role(self, discord_id):
-        guild = self.bot.get_guild(config.lol_server)
-        member = guild.get_member(discord_id)
-        if member:
-            not_eligible_role = discord.utils.get(guild.roles, name="Not Eligible")
-            if not_eligible_role:
-                await member.add_roles(not_eligible_role)
-                logger.info(f"Assigned Not Eligible role to {member.name}")
-            else:
-                logger.error(f"Not Eligible role not found in the server.")
-        else:
-            logger.error(f"Member with discord_id {discord_id} not found.")
+        match_timestamp = match.get('timestamp') / 1000  # Assuming match timestamp is in milliseconds
+        return current_split['start'].timestamp() <= match_timestamp <= (current_split['end'].timestamp() if current_split['end'] else float('inf'))
 
     async def get_puuid(self, game_name, tag_line):
         import urllib.parse
         encoded_game_name = urllib.parse.quote(game_name)
         encoded_tag_line = urllib.parse.quote(tag_line)
 
-        url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
+        url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{encoded_game_name}/{encoded_tag_line}"
         headers = {'X-Riot-Token': config.RIOT_API}
         
         async with aiohttp.ClientSession() as session:
@@ -287,14 +245,6 @@ class PlayerCog(commands.Cog):
                 else:
                     logger.error(f"Error fetching rank info for Summoner ID {summoner_id}: {await response.text()}")
                     return None
-
-    async def report_failure(self, channel, player_name, reason):
-        if channel:
-            message = f"Player: {player_name} | Error: {reason}"
-            await channel.send(message)
-
-    
-
 
 def setup(bot):
     bot.add_cog(PlayerCog(bot))
