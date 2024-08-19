8aafa39c74e8d007c46485f961dfec8d07224e8d
WarmMilkCodes <mrguybanks@gmail.com>
2024-08-18 20:30:15 -0500

    combine commands

--- cogs/player.py
+++ cogs/player.py
@@ -19,70 +19,50 @@ SPLITS = [
 class PlayerCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.rank_update_task_started = False
-        self.eligibility_check_task_started = False
-        self.eligibility_check_running = False
-        logger.info("PlayerCog loaded. Waiting for rank update and eligibility check tasks to be started.")
+        self.task_started = False
+        self.task_running = False
+        logger.info("PlayerCog loaded. Waiting for rank and eligibility check task to be started.")
 
     def cog_unload(self):
-        if self.rank_update_task_started:
-            self.rank_update_task.cancel()
-            logger.info("PlayerCog unloaded and rank task cancelled.")
-        if self.eligibility_check_task_started:
-            self.eligibility_check_task.cancel()
-            logger.info("PlayerCog unloaded and eligibility task cancelled.")
+        if self.task_started:
+            self.rank_and_eligibility_task.cancel()
+            logger.info("PlayerCog unloaded and task cancelled.")
 
-    @commands.slash_command(guild_ids=[config.lol_server], description="Start the rank update task")
+    @commands.slash_command(guild_ids=[config.lol_server], description="Start the rank and eligibility check task")
     @commands.has_permissions(administrator=True)
-    async def start_rank_update_task(self, ctx):
-        if not self.rank_update_task_started:
-            self.rank_update_task.start()
-            self.rank_update_task_started = True
-            await ctx.respond("Rank update task started and will run every 24 hours.", ephemeral=True)
-            logger.info("Rank update task started by admin command.")
+    async def start_rank_and_eligibility_task(self, ctx):
+        if not self.task_started:
+            self.rank_and_eligibility_task.start()
+            self.task_started = True
+            await ctx.respond("Rank and eligibility check task started and will run every 24 hours.", ephemeral=True)
+            logger.info("Rank and eligibility check task started by admin command.")
         else:
-            await ctx.respond("Rank update task is already running.", ephemeral=True)
-
-    @commands.slash_command(guild_ids=[config.lol_server], description="Start the eligibility check task")
-    @commands.has_permissions(administrator=True)
-    async def start_eligibility_check_task(self, ctx):
-        if not self.eligibility_check_task_started:
-            self.eligibility_check_task.start()
-            self.eligibility_check_task_started = True
-            await ctx.respond("Eligibility check task started and will run every 12 hours.", ephemeral=True)
-            logger.info("Eligibility task started by admin command")
-        else:
-            await ctx.respond("Eligibility check task is already running.", ephemeral=True)
+            await ctx.respond("Rank and eligibility check task is already running.", ephemeral=True)
 
     @tasks.loop(hours=24)
-    async def rank_update_task(self):
-        logger.info("Rank update task triggered.")
-        await self.update_all_ranks()
-
-    @tasks.loop(hours=12)
-    async def eligibility_check_task(self):
-        if self.eligibility_check_running:
-            logger.warning("Eligibility check task is already running. Skipping this execution. ")
+    async def rank_and_eligibility_task(self):
+        if self.task_running:
+            logger.warning("Rank and eligibility check task is already running. Skipping this execution.")
             return
         
-        self.eligibility_check_running = True
-        logger.info("Eligibility check task triggered.")
+        self.task_running = True
+        logger.info("Rank and Eligibility update task triggered.")
         
         try:
-            await self.check_player_eligibility()
+            await self.update_ranks_and_check_eligibility()
         finally:
-            self.eligibility_check_running = False
-            logger.info("Eligibility check task completed.")
+            self.task_running = False
+            logger.info("Rank and eligibility check task completed.")
 
-    @commands.slash_command(guild_ids=[config.lol_server], description="Update player ranks manually")
+    @commands.slash_command(guild_ids=[config.lol_server], description="Update player ranks and check eligibility manually")
     @commands.has_permissions(administrator=True)
-    async def update_ranks(self, ctx):
+    async def update_ranks_and_check_eligibility(self, ctx):
         await ctx.defer(ephemeral=True)
-        await self.update_all_ranks()
-        await ctx.respond("Updated ranks for all players", ephemeral=True)
+        await self.update_ranks_and_check_eligibility()
+        await ctx.respond("Updated ranks and checked eligibility for all players.", ephemeral=True)
 
-    async def update_all_ranks(self):
-        logger.info("Updating ranks for all players.")
+    async def update_ranks_and_check_eligibility(self):
+        logger.info("Updating ranks and checking eligibility for all players.")
         players = dbInfo.intent_collection.find({"Playing": "Yes"})
 
         for player in players:
@@ -94,6 +74,8 @@ class PlayerCog(commands.Cog):
                 continue
 
             logger.info(f"Processing player: {player_record['name']}")
+
+            # Update Rank
             if 'game_name' in player_record and 'tag_line' in player_record:
                 puuid = await self.get_puuid(player_record['game_name'], player_record['tag_line'])
                 if puuid:
@@ -104,8 +86,7 @@ class PlayerCog(commands.Cog):
                         )
                         logger.info(f"Stored PUUID for {player_record['name']}")
                 else:
-                    if player_record.get('rank_info'):
-                        logger.warning(f"Failed to retrieve PUUID for {player_record['name']}. Using stored rank info.")
+                    logger.warning(f"Failed to retrieve PUUID for {player_record['name']}. Skipping rank and eligibility update.")
                     continue
 
                 summoner_id = await self.get_summoner_id(puuid)
@@ -117,18 +98,13 @@ class PlayerCog(commands.Cog):
                         )
                         logger.info(f"Stored Summoner ID for player {player_record['name']}")
                 else:
-                    if player_record.get('rank_info'):
-                        logger.warning(f"Failed to retrieve Summoner ID for {player_record['name']}. Using stored rank info.")
+                    logger.warning(f"Failed to retrieve Summoner ID for {player_record['name']}. Skipping rank and eligibility update.")
                     continue
 
                 rank_info = await self.get_player_rank(summoner_id)
                 if rank_info:
                     date_str = datetime.now(pytz.utc).strftime('%m-%d-%Y')
                     historical_rank_info = player_record.get('historical_rank_info', {})
-
-                    if date_str in historical_rank_info and historical_rank_info[date_str] == rank_info:
-                        logger.info(f"Rank information for player {player_record['name']} is unchanged for today.")
-                        continue
 
                     if not player_record.get('rank_info') or player_record['rank_info'] != rank_info:
                         historical_rank_info[date_str] = rank_info
@@ -141,43 +117,14 @@ class PlayerCog(commands.Cog):
                             }}
                         )
                         logger.info(f"Updated rank information for player {player_record['name']} and set last updated.")
-                else:
-                    if player_record.get('rank_info'):
-                        logger.warning(f"Unable to retrieve rank info for {player_record['name']} with API. Using stored rank info.")
-            else:
-                logger.error(f"Player {player_record['name']} does not have game_name and tag_line set.")
 
-        logger.info("Completed updating ranks for all players.")
-
-    async def check_player_eligibility(self):
-        logger.info("Checking player eligibility for the current split.")
-        players = list(dbInfo.intent_collection.find({"Playing": "Yes"}))
-
-        total_players = len(players)
-        logger.info(f"Total players found with 'Playing: Yes': {total_players}")
-
-        for index, player in enumerate(players):
-            discord_id = player.get('ID')
-            logger.info(f"Processing player {index + 1}/{total_players}: discord_id={discord_id}")
-            player_record = dbInfo.player_collection.find_one({"discord_id": discord_id})
-            
-            if not player_record:
-                logger.error(f"Player record not found for discord_id {discord_id}")
-                continue
-
-            logger.info(f"Processing player: {player_record['name']} for eligibility check.")
-            puuid = player_record.get('puuid')
-            if not puuid:
-                logger.warning(f"No PUUID found for player {player_record['name']}. Skipping eligibility check.")
-                continue
-
+            # Check Eligibility after updating rank
             match_history = await self.get_match_history(puuid)
             if match_history is None:
-                logger.error(f"Failed to retrieve match history for {player_record['name']}.")
+                logger.error(f"Failed to retrieve match history for {player_record['name']}. Skipping eligibility check.")
                 continue
 
             eligible_games = [match for match in match_history if self.is_eligible_match(match)]
-
             game_count = len(eligible_games)
             is_eligible = game_count >= 30
 
@@ -194,7 +141,7 @@ class PlayerCog(commands.Cog):
             else:
                 logger.info(f"No change in eligibility for {player_record['name']}")
 
-    logger.info("Completed checking eligibility for all players.")
+        logger.info("Completed rank and eligibility check for all players.")
 
     async def get_match_history(self, puuid):
         url = f"https://<REGION>.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids"
