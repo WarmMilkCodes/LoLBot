30e48c3eb8db4bfcc9e9c736f0df78e68f6ca971
WarmMilkCodes <warmmilkgames@gmail.com>
2024-09-07 23:36:36 -0500

    add (s) to select your role(s)

--- cogs/role_selection.py
+++ cogs/role_selection.py
@@ -88,7 +88,7 @@ class RoleSelectionCog(commands.Cog):
     @commands.has_role("Bot Guy")
     async def post_role_selection(self, ctx):
         view = RoleSelectionView(self.bot, config.riot_id_log_channel)
-        await ctx.send("Select your in-game role:", view=view)
+        await ctx.send("Select your in-game role(s):", view=view)
 
     @commands.Cog.listener()
     async def on_ready(self):
