2f598873206c9efbcf23aa16613ae098361a9c62
WarmMilkCodes <warmmilkgames@gmail.com>
2024-09-10 23:36:38 -0500

    remove eligibility command / use website to check

--- cogs/staff.py
+++ cogs/staff.py
@@ -145,40 +145,6 @@ class StaffCog(commands.Cog):
 
         # Respond with the embed
         await ctx.respond(embed=embed)
-
-        
-    @commands.slash_command(guild_ids=[config.lol_server], description="Return player eligibility tabulate")
-    @commands.has_any_role("Bot Guy", "League Ops")
-    async def player_eligibility(self, ctx):
-        await ctx.defer()
-        # Fetch all players who are playing
-        players = list(dbInfo.player_collection.find({"eligible_for_split": {"$exists": True}}))
-
-        if not players:
-            await ctx.respond("No players found with eligibility information.", ephemeral=True)
-            return
-
-        table_data = []
-        for player in players:
-            discord_id = player.get('discord_id')
-            username = self.bot.get_user(discord_id).name if self.bot.get_user(discord_id) else "Unknown"
-            eligible = "Yes" if player.get("eligible_for_split") else "No"
-            game_count = player.get("current_split_game_count", "N/A")
-            last_check = player.get("last_eligibility_check", "N/A")
-
-            table_data.append([username, eligible, game_count, last_check])
-
-        table_headers = ["Username", "Eligible", "Game Count", "Last Check"]
-        table_string = tabulate(table_data, headers=table_headers, tablefmt="grid")
-
-        if len(table_string) < 2000:
-            await ctx.respond(f"```{table_string}```")
-        else:
-            messages = [table_string[i:i+1990] for i in range(0, len(table_string), 1990)]
-            await ctx.respond(f"```{messages[0]}```")
-        
-        for message in messages:
-            await ctx.followup.send(f"```{message}```")
         
 
     @commands.slash_command(guild_ids=[config.lol_server], description="Update a user's Riot ID")
