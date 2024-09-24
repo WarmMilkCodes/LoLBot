52e20819ec749ff54c7c86104a8ce7aa0b625493
WarmMilk <dylncox@gmail.com>
2024-09-24 17:23:24 -0500

    fix nickname import

--- cogs/salaries.py
+++ cogs/salaries.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import app.dbInfo as dbInfo
 import app.config as config
 import logging
-from utils import update_nickname
+from cogs.utils import update_nickname
 
 logger = logging.getLogger('salary_log')
 
