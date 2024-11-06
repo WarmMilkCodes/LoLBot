c8d6a7e9d02c0384b195709f7f232c8f7440ec17
WarmMilkCodes <mrguybanks@gmail.com>
2024-11-06 00:05:13 -0600

    refactor prep file

--- app/bot.py
+++ app/bot.py
@@ -1,21 +1,19 @@
 import discord, os, asyncio
-import app.config as config
+import config
 from discord.ext import commands
-from app.utils.logging_config import setup_logging
+from utils.logging_config import setup_logging
 
 logger = setup_logging()
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-
 @bot.event
 async def on_ready():
     logger.info(f"Logged in as {bot.user}")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="UR LoL"))
 
-
-# Load all cogs
+# Load cogs
 def load_extensions():
     logger.info("Loading cogs...")
     for filename in os.listdir('./cogs'):
@@ -25,7 +23,6 @@ def load_extensions():
                 logger.info(f'Successfully loaded cogs.{filename[:-3]}')
             except Exception as e:
                 logger.error(f'Error loading cogs.{filename[:-3]}: {e}')
-
 
 async def main():
     async with bot:
