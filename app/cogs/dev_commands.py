c0b1bbd47d3137bf5a1bd9f02c886f0eae81d3cf
WarmMilkCodes <mrguybanks@gmail.com>
2024-12-30 18:34:28 -0600

    collection typo

--- app/cogs/dev_commands.py
+++ app/cogs/dev_commands.py
@@ -33,7 +33,7 @@ class DevCommands(commands.Cog):
             await ctx.defer()  # Defer the response since this can take time
 
             # Fetch all team roles from the database
-            team_roles_cursor = dbInfo.teams_collection.find({}, {"team_id": 1, "_id": 0})
+            team_roles_cursor = dbInfo.team_collection.find({}, {"team_id": 1, "_id": 0})
             team_role_ids = [doc["team_id"] for doc in await team_roles_cursor.to_list(length=None)]
 
             if not team_role_ids:
